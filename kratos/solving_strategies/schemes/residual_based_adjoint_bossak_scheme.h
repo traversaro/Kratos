7a79334ccee544d17673031a076038f568e37c7d
msandre <michael.andre@tum.de>
2018-08-16 13:43:52 +0200

    Fixed typo.

--- kratos/solving_strategies/schemes/residual_based_adjoint_bossak_scheme.h
+++ kratos/solving_strategies/schemes/residual_based_adjoint_bossak_scheme.h
@@ -521,7 +521,7 @@ protected:
             noalias(r_velocity_adjoint) = - r_first_response_gradient - prod(r_first_lhs,r_residual_adjoint);
             noalias(r_acceleration_adjoint) = - r_second_response_gradient - prod(r_second_lhs,r_residual_adjoint);
 
-            // Assemble the contributions to the corresponding nodal unkowns.
+            // Assemble the contributions to the corresponding nodal unknowns.
             unsigned int local_index = 0;
             Geometry< Node<3> >& r_geometry = r_element.GetGeometry();
             for (unsigned int i_node = 0; i_node < r_geometry.PointsNumber(); ++i_node) {
@@ -581,7 +581,7 @@ protected:
 
             noalias(r_adjoint_auxiliary) = - prod(r_second_lhs,r_residual_adjoint) - r_second_response_gradient;
 
-            // Assemble the contributions to the corresponding nodal unkowns.
+            // Assemble the contributions to the corresponding nodal unknowns.
             unsigned int local_index = 0;
             Geometry< Node<3> >& r_geometry = r_element.GetGeometry();
             for (unsigned int i_node = 0; i_node < r_geometry.PointsNumber(); ++i_node) {
