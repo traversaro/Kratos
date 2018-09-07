183979f398c71da2a64c5f987d2fbe1aa84cc514
MFusseder <martin.fusseder@tum.de>
2018-09-07 17:27:03 +0200

    modified test example in order to test new stuff

--- applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_beam_3d2n_structure.py
+++ applications/StructuralMechanicsApplication/tests/test_adjoint_sensitity_analysis_beam_3d2n_structure.py
@@ -5,6 +5,7 @@ from KratosMultiphysics.StructuralMechanicsApplication import *
 import KratosMultiphysics.KratosUnittest as KratosUnittest
 import structural_mechanics_analysis
 import KratosMultiphysics.kratos_utilities as kratos_utilities
+import structural_mechanics_adjoint_analysis
 
 try:
     from KratosMultiphysics.HDF5Application import *
@@ -31,10 +32,6 @@ def solve_primal_problem():
     with open("beam_test_parameters.json",'r') as parameter_file:
         ProjectParametersPrimal = Parameters( parameter_file.read())
 
-    # To avoid many prints
-    if (ProjectParametersPrimal["problem_data"]["echo_level"].GetInt() == 0):
-        Logger.GetDefaultOutput().SetSeverity(Logger.Severity.WARNING)
-
     model_primal = Model()
     primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
     primal_analysis.Run()
@@ -49,19 +46,27 @@ class TestAdjointSensitivityAnalysisBeamStructure(KratosUnittest.TestCase):
     # called only once for this class, opposed of setUp()
     @classmethod
     def setUpClass(cls):
-        with controlledExecutionScope(_get_test_working_dir()):
-            solve_primal_problem()
+        pass
 
     def test_local_stress_response(self):
+        # solve primal problem
+        with controlledExecutionScope(_get_test_working_dir()):
+            with open("beam_test_parameters.json",'r') as parameter_file:
+                ProjectParametersPrimal = Parameters( parameter_file.read())
+
+            model_primal = Model()
+            primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
+            primal_analysis.Run()
+
         #Create the adjoint solver
         with controlledExecutionScope(_get_test_working_dir()):
             with open("beam_test_local_stress_adjoint_parameters.json",'r') as parameter_file:
                 ProjectParametersAdjoint = Parameters( parameter_file.read())
 
             model_part_name = ProjectParametersAdjoint["problem_data"]["model_part_name"].GetString()
-            model_adjoint = Model()
+            #model_adjoint = Model()
 
-            adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, ProjectParametersAdjoint)
+            adjoint_analysis = structural_mechanics_adjoint_analysis.StructuralMechanicsAdjointAnalysis(model_primal, ProjectParametersAdjoint)
             adjoint_analysis.Run()
 
             # Check sensitivities for the parameter I22
@@ -69,22 +74,31 @@ class TestAdjointSensitivityAnalysisBeamStructure(KratosUnittest.TestCase):
             sensitivities_to_check = []
             element_list = [1,6,10]
             for element_id in element_list:
-                sensitivities_to_check.append(model_adjoint.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
-    
+                sensitivities_to_check.append(model_primal.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
+
         self.assertAlmostEqual(sensitivities_to_check[0], reference_values[0], 3)
         self.assertAlmostEqual(sensitivities_to_check[1], reference_values[1], 3)
         self.assertAlmostEqual(sensitivities_to_check[2], reference_values[2], 3)
 
-    def test_nodal_displacement_response(self):
+    def nodal_displacement_response(self):
+        # solve primal problem
+        with controlledExecutionScope(_get_test_working_dir()):
+            with open("beam_test_parameters.json",'r') as parameter_file:
+                ProjectParametersPrimal = Parameters( parameter_file.read())
+
+            model_primal = Model()
+            primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
+            primal_analysis.Run()
+
         # Create the adjoint solver
         with controlledExecutionScope(_get_test_working_dir()):
             with open("beam_test_nodal_disp_adjoint_parameters.json",'r') as parameter_file:
                 ProjectParametersAdjoint = Parameters( parameter_file.read())
 
             model_part_name = ProjectParametersAdjoint["problem_data"]["model_part_name"].GetString()
-            model_adjoint = Model()
+            #model_adjoint = Model()
 
-            adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, ProjectParametersAdjoint)
+            adjoint_analysis = structural_mechanics_adjoint_analysis.StructuralMechanicsAdjointAnalysis(model_primal, ProjectParametersAdjoint)
 
             adjoint_analysis.Run()
 
@@ -93,22 +107,35 @@ class TestAdjointSensitivityAnalysisBeamStructure(KratosUnittest.TestCase):
             sensitivities_to_check = []
             element_list = [1,6,10]
             for element_id in element_list:
-                sensitivities_to_check.append(model_adjoint.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
-           
+                sensitivities_to_check.append(model_primal.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
+
         self.assertAlmostEqual(sensitivities_to_check[0], reference_values[0], 4)
         self.assertAlmostEqual(sensitivities_to_check[1], reference_values[1], 4)
         self.assertAlmostEqual(sensitivities_to_check[2], reference_values[2], 4)
 
-    def test_strain_energy_response(self):
+    def strain_energy_response(self):
+        # solve primal problem
+        with controlledExecutionScope(_get_test_working_dir()):
+            with open("beam_test_parameters.json",'r') as parameter_file:
+                ProjectParametersPrimal = Parameters( parameter_file.read())
+
+            # To avoid many prints
+            if (ProjectParametersPrimal["problem_data"]["echo_level"].GetInt() == 0):
+                Logger.GetDefaultOutput().SetSeverity(Logger.Severity.WARNING)
+
+            model_primal = Model()
+            primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersPrimal)
+            primal_analysis.Run()
+
         # Create the adjoint solver
         with controlledExecutionScope(_get_test_working_dir()):
             with open("beam_test_strain_energy_adjoint_parameters.json",'r') as parameter_file:
                 ProjectParametersAdjoint = Parameters( parameter_file.read())
 
             model_part_name = ProjectParametersAdjoint["problem_data"]["model_part_name"].GetString()
-            model_adjoint = Model()
+            #model_adjoint = Model()
 
-            adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_adjoint, ProjectParametersAdjoint)
+            adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_primal, ProjectParametersAdjoint)
 
             adjoint_analysis.Run()
 
@@ -117,8 +144,8 @@ class TestAdjointSensitivityAnalysisBeamStructure(KratosUnittest.TestCase):
             sensitivities_to_check = []
             element_list = [1,6,10]
             for element_id in element_list:
-                sensitivities_to_check.append(model_adjoint.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
-          
+                sensitivities_to_check.append(model_primal.GetModelPart(model_part_name).Elements[element_id].GetValue(I22_SENSITIVITY))
+
         self.assertAlmostEqual(sensitivities_to_check[0], reference_values[0], 4)
         self.assertAlmostEqual(sensitivities_to_check[1], reference_values[1], 4)
         self.assertAlmostEqual(sensitivities_to_check[2], reference_values[2], 4)
