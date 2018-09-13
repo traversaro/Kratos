edcf00151dcba0d6742f9c62e6fda2197cf1c673
MFusseder <martin.fusseder@tum.de>
2018-09-13 11:06:39 +0200

    cleaning of python running script of example

--- applications/StructuralMechanicsApplication/tests/_2_non-linear-shell-example/run_nonlinear_example.py
+++ applications/StructuralMechanicsApplication/tests/_2_non-linear-shell-example/run_nonlinear_example.py
@@ -5,14 +5,11 @@ from __future__ import print_function, absolute_import, division #makes KratosMu
 from KratosMultiphysics import *
 from KratosMultiphysics.StructuralMechanicsApplication import *
 import structural_mechanics_analysis
-import structural_mechanics_adjoint_analysis
 
 with open("Shell_Q3_Thin_nonlinear_static_test_parameters.json",'r') as parameter_file:
     ProjectParametersPrimal = Parameters( parameter_file.read())
 
-model_part_name_primal = ProjectParametersPrimal["problem_data"]["model_part_name"].GetString()
 my_analysis_model = Model()
-
 primal_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(my_analysis_model, ProjectParametersPrimal)
 primal_analysis.Run()
 
@@ -21,22 +18,7 @@ print("Finished primal problem!")
 with open("Shell_Q3_Thin_nonlinear_static_adjoint_test_parameters.json",'r') as parameter_file:
     ProjectParametersAdjoint = Parameters( parameter_file.read())
 
-#model_part_name = ProjectParametersAdjoint["problem_data"]["model_part_name"].GetString()
-#model_adjoint = Model()
-
-adjoint_analysis = structural_mechanics_adjoint_analysis.StructuralMechanicsAdjointAnalysis(my_analysis_model, ProjectParametersAdjoint)
+adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(my_analysis_model, ProjectParametersAdjoint)
 adjoint_analysis.Run()
-
-
-#print("Finished primal problem!")
-#
-#with open("AdjointParameters.json",'r') as parameter_file:
-#    ProjectParametersAdjoint = Parameters( parameter_file.read())
-#
-#model_part_name_adjoint = ProjectParametersAdjoint["problem_data"]["model_part_name"].GetString()
-#model_truss_adjoint = Model()
-#
-#adjoint_analysis = structural_mechanics_analysis.StructuralMechanicsAnalysis(model_truss_adjoint, ProjectParametersAdjoint)
-#adjoint_analysis.Run()
 
 
