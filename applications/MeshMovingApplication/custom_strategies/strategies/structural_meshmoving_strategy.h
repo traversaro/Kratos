791c8d07236e2070b5572bd2c10e2bb510bb4fb9
philbucher <philipp.bucher@tum.de>
2018-09-21 12:28:15 +0200

    refactored StructuralStrategy

--- applications/MeshMovingApplication/custom_strategies/strategies/structural_meshmoving_strategy.h
+++ applications/MeshMovingApplication/custom_strategies/strategies/structural_meshmoving_strategy.h
@@ -5,260 +5,228 @@
 //                   Multi-Physics
 //
 //  License:		 BSD License
-//					 Kratos default license:
-// kratos/license.txt
+//					 Kratos default license: kratos/license.txt
 //
-//  Main authors:    Andreas Winterstein (a.winterstein@tum.de)
+//  Main authors:    Andreas Winterstein
+//                   Philipp Bucher
 //
 
-#if !defined(KRATOS_NEW_STRUCTURAL_MESHMOVING_STRATEGY)
-#define KRATOS_NEW_STRUCTURAL_MESHMOVING_STRATEGY
+#if !defined(KRATOS_STRUCTURAL_MESHMOVING_STRATEGY_H_INCLUDED )
+#define  KRATOS_STRUCTURAL_MESHMOVING_STRATEGY_H_INCLUDED
 
-/* System includes */
 
-/* External includes */
+// System includes
 
-/* Project includes */
-#include "custom_elements/structural_meshmoving_element.h"
-#include "custom_utilities/move_mesh_utilities.h"
-#include "includes/model_part.h"
-#include "solving_strategies/builder_and_solvers/residualbased_block_builder_and_solver.h"
-#include "solving_strategies/schemes/residualbased_incrementalupdate_static_scheme.h"
-#include "solving_strategies/strategies/residualbased_linear_strategy.h"
-#include "solving_strategies/strategies/solving_strategy.h"
 
-#include "mesh_moving_application.h"
+// External includes
 
-namespace Kratos {
 
-/**@name Kratos Globals */
-/*@{ */
+// Project includes
+#include "base_meshmoving_strategy.h"
 
-/*@} */
-/**@name Type Definitions */
-/*@{ */
 
-/*@} */
+namespace Kratos
+{
+///@addtogroup ApplicationNameApplication
+///@{
 
-/**@name  Enum's */
-/*@{ */
+///@name Kratos Globals
+///@{
 
-/*@} */
-/**@name  Functions */
-/*@{ */
+///@}
+///@name Type Definitions
+///@{
 
-/*@} */
-/**@name Kratos Classes */
-/*@{ */
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
 
 /// Short class definition.
-/**   Detail class definition.
+/** Detail class definition.
  */
 template <class TSparseSpace, class TDenseSpace, class TLinearSolver>
-class StructuralMeshMovingStrategy
-    : public SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver> {
+class StructuralMeshMovingStrategy : public BaseMeshMovingStrategy<TSparseSpace, TDenseSpace, TLinearSolver>
+{
 public:
-  /**@name Type Definitions */
-  /*@{ */
+    ///@name Type Definitions
+    ///@{
 
-  /** Counted pointer of ClassName */
-  KRATOS_CLASS_POINTER_DEFINITION(StructuralMeshMovingStrategy);
+    /// Pointer definition of StructuralMeshMovingStrategy
+    KRATOS_CLASS_POINTER_DEFINITION(StructuralMeshMovingStrategy);
 
-  typedef SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver> BaseType;
-  typedef typename BaseType::TBuilderAndSolverType TBuilderAndSolverType;
-  typedef Scheme<TSparseSpace, TDenseSpace> SchemeType;
+    typedef SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver> BaseType;
+    typedef BaseType::Pointer BaseTypePointer;
 
-  /*@} */
-  /**@name Life Cycle
-   */
-  /*@{ */
+    ///@}
+    ///@name Life Cycle
+    ///@{
 
-  /** Constructor.
-   */
-  StructuralMeshMovingStrategy(ModelPart &model_part,
-                               typename TLinearSolver::Pointer pNewLinearSolver,
-                               int TimeOrder = 2,
-                               bool ReformDofSetAtEachStep = false,
-                               bool ComputeReactions = false,
-                               bool CalculateMeshVelocities = true,
-                               int EchoLevel = 0)
-      : SolvingStrategy<TSparseSpace, TDenseSpace, TLinearSolver>(model_part) {
-    KRATOS_TRY
+    /// Default constructor.
+    StructuralMeshMovingStrategy(ModelPart& rMeshMovingModelPart,
+                                Parameters MeshVolocityCalculationParameters,
+                                const bool ReformDofSetAtEachStep,
+                                BaseTypePointer pStrategy) :
+        BaseMeshMovingStrategy<TSparseSpace, TDenseSpace, TLinearSolver>(
+            rMeshMovingModelPart,
+            MeshVolocityCalculationParameters,
+            ReformDofSetAtEachStep),
+            mpStrategy(pStrategy)
+    { }
 
-    mreform_dof_set_at_each_step = ReformDofSetAtEachStep;
-    mcompute_reactions = ComputeReactions;
-    mcalculate_mesh_velocities = CalculateMeshVelocities;
-    mecho_level = EchoLevel;
-    mtime_order = TimeOrder;
-    bool calculate_norm_dx_flag = false;
+    /// Destructor.
+    ~StructuralMeshMovingStrategy() override {}
 
-    typename SchemeType::Pointer pscheme = typename SchemeType::Pointer(
-        new ResidualBasedIncrementalUpdateStaticScheme<TSparseSpace,
-                                                       TDenseSpace>());
+    ///@}
+    ///@name Operators
+    ///@{
 
-    const std::string element_type = "StructuralMeshMovingElement";
-    mpmesh_model_part = MoveMeshUtilities::GenerateMeshPart(
-        BaseType::GetModelPart(), element_type);
 
-    mpbulider_and_solver = typename TBuilderAndSolverType::Pointer(
-        new ResidualBasedBlockBuilderAndSolver<TSparseSpace, TDenseSpace,
-                                               TLinearSolver>(
-            pNewLinearSolver));
+    ///@}
+    ///@name Operations
+    ///@{
 
-    mstrategy = typename BaseType::Pointer(
-        new ResidualBasedLinearStrategy<TSparseSpace, TDenseSpace,
-                                        TLinearSolver>(
-            *mpmesh_model_part, pscheme, pNewLinearSolver, mpbulider_and_solver,
-            mcompute_reactions, mreform_dof_set_at_each_step,
-            calculate_norm_dx_flag));
+    void Initialize() override
+    {
+        mpStrategy->Initialize();
+    }
 
-    mstrategy->SetEchoLevel(mecho_level);
+    void Clear() override
+    {
+        mpStrategy->Clear();
+    }
 
-    KRATOS_CATCH("")
-  }
+    ///@}
+    ///@name Access
+    ///@{
 
-  virtual ~StructuralMeshMovingStrategy() {}
 
-  void Initialize() override {}
+    ///@}
+    ///@name Inquiry
+    ///@{
 
-  double Solve() override {
-    KRATOS_TRY;
 
-    MoveMeshUtilities::SetMeshToInitialConfiguration(
-        mpmesh_model_part->GetCommunicator().LocalMesh().Nodes());
+    ///@}
+    ///@name Input and output
+    ///@{
 
-    // Solve for the mesh movement
-    mstrategy->Solve();
 
-    // Update FEM-base
-    const double delta_time =
-        BaseType::GetModelPart().GetProcessInfo()[DELTA_TIME];
+    ///@}
+    ///@name Friends
+    ///@{
 
-    if (mcalculate_mesh_velocities == true)
-        MoveMeshUtilities::CalculateMeshVelocities(mpmesh_model_part.get(), mtime_order,
-                                                   delta_time);
-    MoveMeshUtilities::MoveMesh(
-        mpmesh_model_part->GetCommunicator().LocalMesh().Nodes());
 
-    // Clearing the system if needed
-    if (mreform_dof_set_at_each_step == true)
-      mstrategy->Clear();
-
-    return 0.0;
-
-    KRATOS_CATCH("");
-  }
-
-  void UpdateReferenceMesh()
-  {
-  MoveMeshUtilities::UpdateReferenceMesh(BaseType::GetModelPart());
-  }
-
-  /*@} */
-  /**@name Operators
-   */
-  /*@{ */
-
-  /*@} */
-  /**@name Operations */
-  /*@{ */
-
-  /*@} */
-  /**@name Access */
-  /*@{ */
-
-  /*@} */
-  /**@name Inquiry */
-  /*@{ */
-
-  /*@} */
-  /**@name Friends */
-  /*@{ */
-
-  /*@} */
+    ///@}
 
 protected:
-  /**@name Protected static Member Variables */
-  /*@{ */
+    ///@name Protected static Member Variables
+    ///@{
 
-  /*@} */
-  /**@name Protected member Variables */
-  /*@{ */
 
-  /*@} */
-  /**@name Protected Operators*/
-  /*@{ */
+    ///@}
+    ///@name Protected member Variables
+    ///@{
 
-  /*@} */
-  /**@name Protected Operations*/
-  /*@{ */
 
-  /*@} */
-  /**@name Protected  Access */
-  /*@{ */
+    ///@}
+    ///@name Protected Operators
+    ///@{
 
-  /*@} */
-  /**@name Protected Inquiry */
-  /*@{ */
 
-  /*@} */
-  /**@name Protected LifeCycle */
-  /*@{ */
+    ///@}
+    ///@name Protected Operations
+    ///@{
 
-  /*@} */
+    void ComputeMeshMovement() override
+    {
+        mpStrategy->Solve();
+    }
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+
+    ///@}
 
 private:
-  /**@name Static Member Variables */
-  /*@{ */
+    ///@name Static Member Variables
+    ///@{
 
-  /*@} */
-  /**@name Member Variables */
-  /*@{ */
-  std::unique_ptr<ModelPart> mpmesh_model_part;
 
-  typename BaseType::Pointer mstrategy;
-  typename TBuilderAndSolverType::Pointer mpbulider_and_solver;
+    ///@}
+    ///@name Member Variables
+    ///@{
 
-  int mecho_level;
-  int mtime_order;
-  bool mreform_dof_set_at_each_step;
-  bool mcompute_reactions;
-  bool mcalculate_mesh_velocities;
+    BaseTypePointer pStrategy;
 
-  /*@} */
-  /**@name Private Operators*/
-  /*@{ */
+    ///@}
+    ///@name Private Operators
+    ///@{
 
-  /*@} */
-  /**@name Private Operations*/
-  /*@{ */
-  /*@} */
-  /**@name Private  Access */
-  /*@{ */
 
-  /*@} */
-  /**@name Private Inquiry */
-  /*@{ */
+    ///@}
+    ///@name Private Operations
+    ///@{
 
-  /*@} */
-  /**@name Un accessible methods */
-  /*@{ */
 
-  /** Copy constructor.
-   */
-  StructuralMeshMovingStrategy(const StructuralMeshMovingStrategy &Other);
+    ///@}
+    ///@name Private  Access
+    ///@{
 
-  /*@} */
 
-}; /* Class StructuralMeshMovingStrategy */
+    ///@}
+    ///@name Private Inquiry
+    ///@{
 
-/*@} */
 
-/**@name Type Definitions */
-/*@{ */
+    ///@}
+    ///@name Un accessible methods
+    ///@{
 
-/*@} */
-}
-/* namespace Kratos.*/
+    /// Assignment operator.
+    StructuralMeshMovingStrategy& operator=(StructuralMeshMovingStrategy const& rOther){}
 
-#endif /* KRATOS_NEW_STRUCTURAL_MESHMOVING_STRATEGY  defined */
+    /// Copy constructor.
+    StructuralMeshMovingStrategy(StructuralMeshMovingStrategy const& rOther){}
+
+
+    ///@}
+
+}; // Class StructuralMeshMovingStrategy
+
+///@}
+
+///@name Type Definitions
+///@{
+
+
+///@}
+///@name Input and output
+///@{
+
+
+///@}
+
+///@} addtogroup block
+
+}  // namespace Kratos.
+
+#endif // KRATOS_STRUCTURAL_MESHMOVING_STRATEGY_H_INCLUDED  defined
