5d47e7dd41ffb76aacf4621dc4f9274c7a2e7fc7
philbucher <philipp.bucher@tum.de>
2018-09-28 07:31:00 +0200

    added empty main test-file

---
+++ applications/EmpireApplication/tests/test_EmpireApplication.py
@@ -0,0 +1,39 @@
+# import Kratos
+import KratosMultiphysics
+import KratosMultiphysics.EmpireApplicationApplication
+
+# Import Kratos "wrapper" for unittests
+import KratosMultiphysics.KratosUnittest as KratosUnittest
+
+
+def AssembleTestSuites():
+    ''' Populates the test suites to run.
+
+    Populates the test suites to run. At least, it should pupulate the suites:
+    "small", "nighlty" and "all"
+
+    Return
+    ------
+
+    suites: A dictionary of suites
+        The set of suites with its test_cases added.
+    '''
+
+    suites = KratosUnittest.KratosSuites
+
+    smallSuite = suites['small']
+
+    nightSuite = suites['nightly']
+    nightSuite.addTests(smallSuite)
+
+    validationSuite = suites['validation']
+
+    # Create a test suit that contains all the tests:
+    allSuite = suites['all']
+    allSuite.addTests(nightSuite) # already contains the smallSuite
+    allSuite.addTests(validationSuite)
+
+    return suites
+
+if __name__ == '__main__':
+    KratosUnittest.runTests(AssembleTestSuites())
