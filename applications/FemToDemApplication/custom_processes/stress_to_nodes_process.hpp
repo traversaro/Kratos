964454a4200841475b4cf514a00fdc1299b28b95
Alejandro Cornejo Velázquez <alejandro.cornejo23@gmail.com>
2018-10-09 18:02:07 +0200

    formatting process

--- applications/FemToDemApplication/custom_processes/stress_to_nodes_process.hpp
+++ applications/FemToDemApplication/custom_processes/stress_to_nodes_process.hpp
@@ -88,40 +88,27 @@ class StressToNodesProcess : public Process
         double damage;
 
         // Loop over elements to extrapolate the stress to the nodes
-        for (ElementsArrayType::ptr_iterator it = mr_model_part.Elements().ptr_begin(); it != mr_model_part.Elements().ptr_end(); ++it)
-        {
+        for (ElementsArrayType::ptr_iterator it = mr_model_part.Elements().ptr_begin(); it != mr_model_part.Elements().ptr_end(); ++it) {
 
             bool condition_is_active = true;
-            if ((*it)->IsDefined(ACTIVE))
-            {
+            if ((*it)->IsDefined(ACTIVE)) {
                 condition_is_active = (*it)->Is(ACTIVE);
             }
 
-            if (condition_is_active)
-            {
+            if (condition_is_active) {
 
-                if ((*it)->GetGeometry().PointsNumber() == 3)
-                {
+                if ((*it)->GetGeometry().PointsNumber() == 3) {
                     GaussPointsStresses = (*it)->GetValue(STRESS_VECTOR);
-                    //damage = (*it)->GetValue(DAMAGE_ELEMENT);
-
-                    for (int i = 0; i < 3; i++)
-                    {
+                    for (int i = 0; i < 3; i++) {
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[0] += GaussPointsStresses[0];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[1] += GaussPointsStresses[1];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[3] += GaussPointsStresses[2];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].NElems += 1;
-                        //pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id()-1].Damage += damage;
                     }
-                }
-                else
-                {
+                } else {
                     GaussPointsStresses = (*it)->GetValue(STRESS_VECTOR);
-					//KRATOS_WATCH(GaussPointsStresses)
-                    //damage = (*it)->GetValue(DAMAGE_ELEMENT);
 
-                    for (int i = 0; i < 4; i++)
-                    {
+                    for (int i = 0; i < 4; i++) {
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[0] += GaussPointsStresses[0];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[1] += GaussPointsStresses[1];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[2] += GaussPointsStresses[2];
@@ -129,49 +116,40 @@ class StressToNodesProcess : public Process
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[4] += GaussPointsStresses[4];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].EffectiveStressVector[5] += GaussPointsStresses[5];
                         pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id() - 1].NElems += 1;
-                        //pNodeStressesVector[(*it)->GetGeometry().GetPoint(i).Id()-1].Damage += damage;
                     }
                 }
             }
         }
 
         // Ponderate over the elements coincident on that node
-        for (unsigned int i = 0; i < mNNodes; i++)
-        {
+        for (unsigned int i = 0; i < mNNodes; i++) {
             pNodeStressesVector[i].EffectiveStressVector = pNodeStressesVector[i].EffectiveStressVector / pNodeStressesVector[i].NElems;
             //pNodeStressesVector[i].Damage = pNodeStressesVector[i].Damage / pNodeStressesVector[i].NElems;
         }
 
         // Loop to compute the max eq. stress in order to normalize
-        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it)
-        {
-
+        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it) {
             int Id = (*it).Id();
             const Vector nodal_stress = pNodeStressesVector[Id - 1].EffectiveStressVector;
 
             // Compute the norm of the vector -> Put VonMises stress
             double &norm = it->GetSolutionStepValue(EQUIVALENT_NODAL_STRESS);
             norm = this->CalculateStressInvariant(nodal_stress);
-
-            //double& damage = it->GetSolutionStepValue(NODAL_DAMAGE);
-            //damage = pNodeStressesVector[Id - 1].Damage;
         }
 
         // Loop to compute the max eq. stress and normalize
         double MaxEqStress = 0.0;
-        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it)
-        {
+        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it) {
 
             double &norm = it->GetSolutionStepValue(EQUIVALENT_NODAL_STRESS);
             if (norm > MaxEqStress)
                 MaxEqStress = norm;
         }
 
-        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it)
-        {
+        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it) {
             double &norm = it->GetSolutionStepValue(EQUIVALENT_NODAL_STRESS);
-            // norm /= (MaxEqStress * 1.0e4);
-            norm /= (MaxEqStress);
+            norm /= (MaxEqStress * 1.0e4);
+            // norm /= (MaxEqStress);
         }
     }
 
@@ -198,8 +176,7 @@ class StressToNodesProcess : public Process
         int aux = 0;
         int id;
 
-        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it)
-        {
+        for (ModelPart::NodeIterator it = mr_model_part.NodesBegin(); it != mr_model_part.NodesEnd(); ++it) {
             id = (*it).Id();
             if (id > aux)
                 aux = id;
