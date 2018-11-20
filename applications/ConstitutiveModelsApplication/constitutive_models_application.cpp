25b938257a1839f77dabe6a56b36bf28195e60d6
Josep Maria <cpuigbo@cimne.upc.edu>
2018-11-20 11:29:11 +0100

    name clash hotfix in  constitutive laws

--- applications/ConstitutiveModelsApplication/constitutive_models_application.cpp
+++ applications/ConstitutiveModelsApplication/constitutive_models_application.cpp
@@ -14,9 +14,7 @@
 
 
 // Project includes
-#include "includes/define.h"
 #include "constitutive_models_application.h"
-#include "includes/variables.h"
 
 
 namespace Kratos {
@@ -68,8 +66,8 @@ namespace Kratos {
     //strain rate laws
     KRATOS_REGISTER_CONSTITUTIVE_LAW( "StrainRate3DLaw", mStrainRate3DLaw );
     KRATOS_REGISTER_CONSTITUTIVE_LAW( "StrainRatePlaneStrain2DLaw", mStrainRatePlaneStrain2DLaw );
-    KRATOS_REGISTER_CONSTITUTIVE_LAW( "Newtonian3DLaw", mNewtonian3DLaw );
-    KRATOS_REGISTER_CONSTITUTIVE_LAW( "NewtonianPlaneStrain2DLaw", mNewtonianPlaneStrain2DLaw );
+    KRATOS_REGISTER_CONSTITUTIVE_LAW( "NewtonianFluid3DLaw", mNewtonianFluid3DLaw );
+    KRATOS_REGISTER_CONSTITUTIVE_LAW( "NewtonianFluidPlaneStrain2DLaw", mNewtonianFluidPlaneStrain2DLaw );
 
     //general constitutive models
 
